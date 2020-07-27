7303e42d122c73bcc4e1f91340f5e62c2f8aa84c
Pradeep Dahal <pradeep.dahal222@gmail.com>
2020-07-27 22:57:47 +0545

    upto jquery animation

---
+++ Web Development Crash Course/Jquery/main.js
@@ -0,0 +1,44 @@
+
+// $(document).ready(function (){
+//     $('h1').css('color','red')
+// });
+
+// console.log($('h1').css('color'))
+
+// $('h1').addClass('big-title margin-50');
+
+// $('h1').text('This is changed form the ')
+
+// $('button').text('Button')
+// $('button').html('<i>This is</i>')
+// $('h1').click(function(){
+//   $('h1').css('color','purple')
+// })
+
+
+// $('button').click(function(){
+//   $('h1').css('color','purple')
+// });
+
+// $(document).keypress(function (event){
+//   $('h1').text(event.key)
+// })
+// $('h1').on("mouseover", function(){
+//   console.log('something')
+//   $('h1').css('color','purple')
+//
+// });
+// $('h1').on("mouseout", function(){
+//   console.log('something')
+//   $('h1').css('color','black')
+//
+// });
+// $('h1').after('<button>New</button>')
+
+// $('button').click(function(){
+//   $('h1').slideToggle()
+// });
+
+// $('button').click(function(){
+//   $('h1').animate({opacity:0.5})
+// });
