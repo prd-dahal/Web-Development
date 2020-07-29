c5ea35661f8a73ea3b813a52918a3b382acc8da4
Pradeep Dahal <pradeep.dahal222@gmail.com>
2020-07-29 15:22:11 +0545

    bmi calculator

--- Web Development Crash Course/Calculator/calculator.js
+++ Web Development Crash Course/Calculator/calculator.js
@@ -13,7 +13,17 @@ app.post('/', function(req,res){
   total = Number(req.body.num1) + Number(req.body.num2)
   res.send('The result of the calculation is '+ total)
 })
+
+app.get('/bmi',function(req,res){
+  res.sendFile(__dirname+'/bmiCalculator.html');
+});
+
+app.post('/bmi', function(req, res){
+  weight = Number(req.body.weight);
+  height = Number(req.body.height);
+  bmi = weight/height;
+  res.send('Your BMI is '+ bmi)
+})
 app.listen(3000, function(){
   console.log('Server started at 3000')
 });
- 
