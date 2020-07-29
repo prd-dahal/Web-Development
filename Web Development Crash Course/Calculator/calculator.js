c806ddecfac63faac3d302cbc225714d6d9f60a1
Pradeep Dahal <pradeep.dahal222@gmail.com>
2020-07-29 15:13:53 +0545

    simple calculator with client side processing

--- Web Development Crash Course/Calculator/calculator.js
+++ Web Development Crash Course/Calculator/calculator.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/',function(req, res){
-  res.send("Hello world")
+
+  res.sendFile(__dirname+'/index.html');
 });
 
+app.post('/', function(req,res){
+  total = Number(req.body.num1) + Number(req.body.num2)
+  res.send('The result of the calculation is '+ total)
+})
 app.listen(3000, function(){
   console.log('Server started at 3000')
 });
+ 
