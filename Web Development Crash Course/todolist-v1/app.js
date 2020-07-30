5149a84af4222348a7a26cb5b03ced59a3c8a670
Pradeep Dahal <pradeep.dahal222@gmail.com>
2020-07-30 19:30:06 +0545

    first ejs templating

--- Web Development Crash Course/todolist-v1/app.js
+++ Web Development Crash Course/todolist-v1/app.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 app = express()
-
+app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('public'))
+
 app.get('/', function(req, res){
-  res.send("Oh boy it worked")
+  var today = new Date()
+  currentDay = today.getDay()
+  day = ""
+  switch(currentDay){
+    case 0:
+      day = 'Sunday';
+      break
+   case 1:
+    day = 'Monday'
+    break
+  case 2:
+    day = 'Tuesday'
+    break
+  case 3:
+    day = 'Wednesday'
+    break
+  case 4:
+    day = 'Thursday'
+    break
+  case 5:
+    day = 'Friday'
+    break
+  case 6:
+    day ='Saturday'
+    break
+  default:
+    console.log("Invalid value")
+  }
+  res.render('list',{day:day})
 })
 
 app.listen(3000,function(){
