32d1527b517efd6a09d08cec90da5660f58e5b5d
Pradeep Dahal <pradeep.dahal222@gmail.com>
2020-08-03 14:17:52 +0545

    crud in mongoose

--- Web Development Crash Course/FruitsDatabase/app.js
+++ Web Development Crash Course/FruitsDatabase/app.js
@@ -2,15 +2,53 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/peopleDB', {useNewUrlParser:true,useUnifiedTopology: true})
 
 const peopleSchema = new mongoose.Schema({
-  name:String,
-  age:Number
+  name:{
+    type:String,
+    required:true
+  },
+  age:{
+    type:Number,
+    min:0,
+    max:130,
+    required:true
+  }
 });
 
 const People = mongoose.model('people',peopleSchema)
-// const people = new People({
-//   name:"Pradeep",
-//   age:22,
+const people = new People({
+  name:"Pradeep",
+  age:12
+
+})
+// people.save()
+
+//UPDATE RECORD
+// People.updateOne({name:"Ajay"},{age:30},function (err){
+//   if(err){
+//     console.log(err)
+//   }else{
+//     console.log("Updated Succesfully")
+//     mongoose.connection.close()
+//   }
 // })
+
+//DELETE RECORD
+// People.deleteOne({name:"Ajay"}, function(err){
+//   if(err){
+//     console.log(err)
+//   }else{
+//     console.log("Succesfully deleted record")
+//   }
+// })
+//  DELETE MULTIPLE RECORD
+People.deleteMany({name:"Pradeep"}, function(err){
+  if(err){
+    console.log(err)
+  }else{
+    console.log("Deleted")
+  }
+})
+
 // const people2 = new People({
 //     name:"Ranjana",
 //     age:25
@@ -26,13 +64,17 @@ const People = mongoose.model('people',peopleSchema)
 //     console.log('Successfully inserted the peoples')
 //   }
 // })
-// people.save()
+
+
+
+
 People.find(function(err, peoples){
   if(err){
     console.log(err)
   }else{
+    mongoose.connection.close()
     peoples.forEach(function(items, index){
-      console.log(items.name)
+    console.log(items.name)
     })
   }
 })
