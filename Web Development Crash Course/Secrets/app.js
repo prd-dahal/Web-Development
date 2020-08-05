2da652e2043fe0f9cfa2b6819173f38f22bae593
Pradeep Dahal <pradeep.dahal222@gmail.com>
2020-08-05 15:42:55 +0545

    initial server setup

--- Web Development Crash Course/Secrets/app.js
+++ Web Development Crash Course/Secrets/app.js
@@ -1,1 +1,18 @@
 //jshint esversion:6
+express = require('express')
+ejs = require('ejs')
+bodyParser = require('body-parser')
+mongoose = require('mongoose')
+
+app = express()
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.get('/',function(req,res){
+  res.render('home')
+})
+
+app.listen(3000, function(){
+  console.log("Server started at port 3000")
+})
