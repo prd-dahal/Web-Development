1b7933e77e5bac78bf30ba5c7a4a15b1cf50cdaf
Pradeep Dahal <pradeep.dahal222@gmail.com>
2020-08-05 12:46:02 +0545

    upto post request in rest api

---
+++ Web Development Crash Course/Wiki-API/app.js
@@ -0,0 +1,42 @@
+express = require('express')
+bodyParser = require('body-parser')
+ejs = require('ejs')
+mongoose = require('mongoose')
+
+app = express()
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+mongoose.connect('mongodb://localhost:27017/wikiDB',{useNewUrlParser:true})
+const articleSchema = new mongoose.Schema({
+  title:String,
+  content:String
+})
+
+const Article = mongoose.model('Article',articleSchema)
+
+app.get('/articles', function(req,res){
+  Article.find(function(err, foundArticles){
+      if(err){
+        console.log(err)
+      }
+      else{
+        res.send(foundArticles)
+      }
+  })
+
+})
+app.post('/articles',function(req,res){
+  console.log(req.body.title)
+  console.log(req.body.content)
+  const newArticle = new Article({
+    title:req.body.title,
+    content:req.body.title
+  })
+  newArticle.save()
+})
+
+app.listen(3000,function(){
+  console.log("Server started at port 3000")
+})
