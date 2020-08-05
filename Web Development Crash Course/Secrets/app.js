73506f15a9b25fc8d8ca1991660657f20a2c723f
Pradeep Dahal <pradeep.dahal222@gmail.com>
2020-08-05 17:40:04 +0545

    environment variable

--- Web Development Crash Course/Secrets/app.js
+++ Web Development Crash Course/Secrets/app.js
@@ -1,9 +1,11 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const encrypt = require('mongoose-encryption')
+
 
 app = express()
 app.use(bodyParser.urlencoded({extended:true}))
@@ -17,9 +19,9 @@ const userSchema = new mongoose.Schema({
   email:String,
   password:String
 })
-const secret = "Thisisourlittlesecret."
 
-userSchema.plugin(encrypt, {secret:secret, encryptedFields:['password']})
+
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:['password']})
 
 const User = new mongoose.model('User',userSchema)
 
